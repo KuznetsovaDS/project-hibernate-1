6c36327bec4a61cfa504371fc15b2b877ba42334
Darya Kuznetsova <Dashar1996@gmail.com>
2025-05-20 11:51:25 +0300

    change NamedQuery

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -8,6 +8,7 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
@@ -39,9 +40,8 @@ public class PlayerRepositoryDB implements IPlayerRepository {
     @Override
     public int getAllCount() {
         try(Session session = sessionFactory.openSession()){
-            Long count = session.createNamedQuery("Player.getAllCount", Long.class)
-                    .getSingleResult();
-            return count.intValue();
+            Query<Long> query = session.createNamedQuery("Player.getAllCount", Long.class);
+            return Math.toIntExact(query.uniqueResult());
         }
     }
     @Override
